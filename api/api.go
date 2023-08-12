12605e8d219beffc015940640e6d651717cfe6b8
Jakub Kovarik <jakubkovarik@seznam.cz>
2023-08-12 10:32:50 +0200

    refactoring of main

---
+++ api/api.go
@@ -0,0 +1,45 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+)
+
+type Response map[string]any
+type ErrorResponse struct {
+	Error string `json:"error"`
+}
+
+func sendJson(w http.ResponseWriter, d Response, err error) {
+	w.Header().Set("Content-Type", "application/json")
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		errorRespone := ErrorResponse{Error: err.Error()}
+		errorResponseJson, err := json.Marshal(errorRespone)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Write(errorResponseJson)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(d)
+}
+
+func getResponse(data any, err error) (Response, error) {
+	response := make(Response)
+
+	response["date"] = time.Now().UTC().Format("2006-01-02T15:04:05.000Z")
+	response["data"] = data
+
+	return response, err
+}
+
+func SendResponse(w http.ResponseWriter, data any, err error) {
+	response, err := getResponse(data, err)
+	sendJson(w, response, err)
+}
--- main.go
+++ main.go
@@ -1,64 +1,28 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	"kovaja/sun-forecast/api"
 	"kovaja/sun-forecast/weather"
 	"log"
 	"net/http"
-	"time"
 
 	"github.com/joho/godotenv"
 )
 
 const DEFAULT_API_PATH = "/api/"
 
-type Response map[string]any
-type ErrorResponse struct {
-	Error string `json:"error"`
-}
-
-func getResponse(data any, err error) (Response, error) {
-	response := make(Response)
-
-	response["date"] = time.Now().UTC().Format("2006-01-02T15:04:05.000Z")
-	response["data"] = data
-
-	return response, err
-}
-
-func sendJson(w http.ResponseWriter, d Response, err error) {
-	w.Header().Set("Content-Type", "application/json")
-
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		errorRespone := ErrorResponse{Error: err.Error()}
-		errorResponseJson, err := json.Marshal(errorRespone)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		w.Write(errorResponseJson)
-		return
-	}
-
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(d)
-}
-
 func defaultPathHandler(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, DEFAULT_API_PATH, http.StatusMovedPermanently)
 }
 
-func apiHandler(w http.ResponseWriter, r *http.Request) {
-	response, err := getResponse(nil, nil)
-	sendJson(w, response, err)
+func defaultApiHandler(w http.ResponseWriter, r *http.Request) {
+	api.SendResponse(w, nil, nil)
 }
 
 func weatherHandler(w http.ResponseWriter, r *http.Request) {
 	data, err := weather.GetFourDays()
-	response, err := getResponse(data, err)
-	sendJson(w, response, err)
+	api.SendResponse(w, data, err)
 }
 
 func checkError(err error) {
@@ -72,7 +36,7 @@ func main() {
 	checkError(godotenv.Load())
 
 	http.HandleFunc(DEFAULT_API_PATH+"weather/", weatherHandler)
-	http.HandleFunc(DEFAULT_API_PATH, apiHandler)
+	http.HandleFunc(DEFAULT_API_PATH, defaultApiHandler)
 	http.HandleFunc("/", defaultPathHandler)
 
 	checkError(http.ListenAndServe(":8080", nil))
