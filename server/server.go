9542b18aa49517405695b9962361db7613e27c87
Jakub Kovarik <jakubkovarik@seznam.cz>
2023-08-18 19:43:54 +0200

    debug update handler

--- server/server.go
+++ server/server.go
@@ -41,7 +41,8 @@ func consumeForecastHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func updateForecastHandler(w http.ResponseWriter, r *http.Request) {
-	logger.Log("Update forecast api called %s", r.Method)
+	// logger.Log("Update forecast api called %s | %s | %s | %s | %v", r.Method, r.URL, r.RequestURI, r.RemoteAddr, r.TLS)
+	logger.Log("Update forecast api called %v", r)
 
 	var err error
 	if r.Method == http.MethodPost {
