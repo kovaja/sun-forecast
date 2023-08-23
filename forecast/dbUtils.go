c15cf79a2635f1b06876cbaf93f7ff1f55dab821
Jakub Kovarik <jakubkovarik@seznam.cz>
2023-08-23 22:14:20 +0200

    start db utils for forecast package

---
+++ forecast/dbUtils.go
@@ -0,0 +1,23 @@
+package forecast
+
+import (
+	"database/sql"
+	"kovaja/sun-forecast/logger"
+	"time"
+)
+
+func getExistingForecast(db *sql.DB, timestamp time.Time) *Forecast {
+	query := "SELECT * FROM forecasts WHERE period_end = $1"
+
+	var forecast Forecast
+	err := db.QueryRow(query, timestamp).Scan(&forecast.Id, &forecast.PeriodEnd, &forecast.Value, &forecast.Actual, &forecast.ActualCount, &forecast.LastActualAt)
+	if err == sql.ErrNoRows {
+		logger.LogError("Did not find existing actuals", err)
+		return nil
+	} else if err != nil {
+		logger.LogError("Failed to load existing forecast", err)
+		return nil
+	}
+
+	return &forecast
+}
--- forecast/utils.go
+++ forecast/utils.go
@@ -7,22 +7,6 @@ import (
 	"time"
 )
 
-func getExistingForecast(db *sql.DB, timestamp time.Time) *Forecast {
-	query := "SELECT * FROM forecasts WHERE period_end = $1"
-
-	var forecast Forecast
-	err := db.QueryRow(query, timestamp).Scan(&forecast.Id, &forecast.PeriodEnd, &forecast.Value, &forecast.Actual, &forecast.ActualCount, &forecast.LastActualAt)
-	if err == sql.ErrNoRows {
-		logger.LogError("Did not find existing actuals", err)
-		return nil
-	} else if err != nil {
-		logger.LogError("Failed to load existing forecast", err)
-		return nil
-	}
-
-	return &forecast
-}
-
 func getPeriodEnd(record HaHistoryRecord) time.Time {
 	inputTime := record.LastChanged
 	minuteDiff := 60 - inputTime.Minute()
