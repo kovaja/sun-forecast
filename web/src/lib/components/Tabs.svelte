bba613bbe7cddc48cc2f5071170e66df25eb775f
Jakub Kovarik <jakubkovarik@seznam.cz>
2023-09-05 19:22:38 +0200

    hide tabs on narrow view

--- web/src/lib/components/Tabs.svelte
+++ web/src/lib/components/Tabs.svelte
@@ -3,18 +3,60 @@
   import { Link } from 'svelte-routing';
 
   export let links: AppLink[];
+
+  let tabsOpened = false
+
+  function toggleTabs (e: MouseEvent) {
+    e.preventDefault()
+    tabsOpened = true
+  }
+
+  function closeTabs () {
+    tabsOpened = false
+  }
 </script>
 
-<nav>
-    {#each links as appLink}
-        <Link to="{appLink.route}">
-            {appLink.name}
-        </Link>
-    {/each}
+<nav class={'tabs-full' + (tabsOpened ? ' tabs-opened' : '')}>
+        {#each links as appLink}
+            <Link to="{appLink.route}" on:click={closeTabs}>
+                {appLink.name}
+            </Link>
+        {/each}
+</nav>
+
+<nav class={'tabs-mobile-toggle' + (tabsOpened ? ' tabs-opened' : '')}>
+    <button on:click={toggleTabs}>
+        ------V-----
+    </button>
 </nav>
 
 <style>
+    .tabs-full {
+        display: none;
+        &.tabs-opened {
+            display: flex;
+        }
+    }
 
+    .tabs-mobile-toggle.tabs-opened {
+        display: none;
+    }
+
+    .tabs-mobile-toggle button {
+        font-size: 8px;
+        padding: 0;
+        border: none;
+        text-align: center;
+    }
+
+    @media (min-width: 480px) {
+        .tabs-mobile-toggle {
+            display: none;
+        }
+        .tabs-full {
+            display: flex;
+        }
+    }
 
     /*Rest of the styles in app.css*/
 </style>
